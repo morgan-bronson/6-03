0d2a24a60a689e2ad02898bad5c31e670c9e0691
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-18 14:52:36 +0000

    else if// else// if

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict'
 
-document.getElementById('mybtn').addEventListener('click', Age)
+document.getElementById('mybtn').addEventListener('click', ageofuser)
 
-function Age() {
-  let age = input
+function ageofuser() {
+  const input = (document.getElementById('input').value)
+  if (input >= 17) {
+    alert('you can see an R rated movie alone')
+  } else if (input >= 13) {
+    alert('you can see a PG-13 movie alone')
+  } else if (input >= 5) {
+    alert('you can see a G or PG movie alone')
+  } else {
+    alert('you cannot see any movies alone')
+  }
